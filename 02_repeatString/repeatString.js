d5ebbee79eeaf26b281137433b1c11bf942d8661
juiceman1014 <justintran1014@gmail.com>
2023-07-30 14:45:55 -0700

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+//General: create a function that takes two arguments, a string and a number, and will repeat
+//the string the given number of times
+//Parameters: string, num
+//Specific:
+//1. for i that's less than the given number
+    //1. display the string
 
+
+const repeatString = function(input, num) {
+
+    let text = "";
+
+    if (num < 0){
+        return "ERROR";
+    }
+
+    for (let i = 0; i < num; i++){
+        text = text + input;
+    }
+
+    return text;
 };
 
 // Do not edit below this line
