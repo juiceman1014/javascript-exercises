9b5e32ca420b2b98d58f89f5b74354ad8ebef036
juiceman1014 <justintran1014@gmail.com>
2023-07-30 15:35:17 -0700

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
-const reverseString = function() {
+//General: take a string and then reverse it;
+//index starts at the end of the string and then after each iteration, a letter is printed to the output
+//Paramters: string
+//specifics
+
+
+
+const reverseString = function(text) {
+    myArray = text.split("");
+    let myString = "";
+
+    for(let i = myArray.length - 1; i > -1; i--){
+        myString = myString + myArray[i];
+    }
+
+    return myString;
 
 };
 
