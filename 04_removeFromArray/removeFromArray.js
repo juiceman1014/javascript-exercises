03807a32728777bf2dc0225c925e2a9b4ec801d1
juiceman1014 <justintran1014@gmail.com>
2023-07-30 18:06:51 -0700

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+//General: make a function that takes an array and removes a certain element of that array.
+//parameters: array, element to be removed
+//specific: iterate through the array and if an element matches the one we're searching for. remove it.
+//1.for loop that checks each element of an array
+//2.if element matches condition, remove it
 
-};
+const removeFromArray = function(array, ...elementsRemoved) {
+return array.filter(item => !elementsRemoved.includes(item))}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
