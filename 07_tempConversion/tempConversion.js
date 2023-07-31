d47268180e3ae69174d32d8ce502646f5de42442
juiceman1014 <justintran1014@gmail.com>
2023-07-31 16:21:21 -0700

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+//General: both functions should take a value, either celsius or fahrenheit
+//Parameters: fahrenheit, celsius
+//Speicifc:
+//1. set up the formula
+//2. put the formula's value into Math.round(num*10)/10
+//3. return the rounded value
+
+
+const convertToCelsius = function(fahrenheit) {
+  let celsius;
+  celsius = (fahrenheit - 32) * (5/9);
+  let roundedCelsius = Math.round(celsius * 10)/10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit;
+  fahrenheit = (celsius * (9/5))+32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10)/10;
+  return roundedFahrenheit;
+
 };
 
 // Do not edit below this line
