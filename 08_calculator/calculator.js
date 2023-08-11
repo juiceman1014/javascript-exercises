178a61f8605d315d3b12d354e9a9485f08c1a77f
juiceman1014 <justintran1014@gmail.com>
2023-08-11 14:41:16 -0700

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(let i = 0; i <array.length; i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
 
+
+const multiply = function(array) {
+  let product = 1;
+  for(let i = 0; i < array.length; i++){
+    product = product * array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let power = Math.pow(num1, num2);
+  return power;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  let result = num;
+  if(num===0 || num ===1){
+    return 1;
+  } else{
+    while(num>1){
+      num--;
+      result = result * num;
+    }
+  }
+return result;
 };
 
 // Do not edit below this line
