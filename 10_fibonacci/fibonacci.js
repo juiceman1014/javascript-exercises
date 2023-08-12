1a6d5bf3fed5368e3c6bfe727e522553b2b451da
juiceman1014 <justintran1014@gmail.com>
2023-08-11 20:15:59 -0700

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(fibNumber) {
+
+
+    const fibSequence = [0,1]
+
+    if(isNaN(fibNumber) == true){
+        fibNumber = Number(fibNumber);
+    }
+
+    if(fibNumber < 1){
+        return "OOPS"
+    }
+
+    for(let i = 2; i < fibNumber +  1; i++){
+        const nextFib = fibSequence[i-1] + fibSequence[i-2];
+        fibSequence.push(nextFib);
+    }
+    
+    return fibSequence[fibNumber];
+
+
 
 };
 
