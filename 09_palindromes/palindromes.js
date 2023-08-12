2d2ca2eec0a632de189c122c935177d508a4177e
juiceman1014 <justintran1014@gmail.com>
2023-08-11 19:44:14 -0700

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let newPhrase = phrase.toLowerCase().replace(/[^a-z0-9]/g, '');
+    console.log(newPhrase);
+    let reversedPhrase = newPhrase.split('').reverse().join("").replace(/[^a-z0-9]/g, '');
+    console.log(reversedPhrase);
+    return newPhrase === reversedPhrase;
 };
 
 // Do not edit below this line
